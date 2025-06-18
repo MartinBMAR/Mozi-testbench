d023f9e6822f4c1cedce01cf179a087b3f1f42fd
Martin <martinbuenodev@gmail.com>
2025-06-18 07:28:25 -0400

    Initial Commit

---
+++ TestBetaStuff/TestBetaStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBetaStuffApp.swift
+//  TestBetaStuff
+//
+//  Created by Martin Bueno on 18/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TestBetaStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
